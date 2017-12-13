ea85dcb536ed14c140d515d4a00eddb5f14baac0
Sergey Shandar <sergey@live.com.au>
2017-12-13 15:14:23 -0800

    0.0.1

--- index.ts
+++ index.ts
@@ -11,42 +11,48 @@ export function createMap(): BagMap {
     function mapGet<T>(bag: Aliq.Bag<T>): Iterable<T>|undefined {
         return <Iterable<T>>cache.get(bag)
     }
-    function createFlatMap<T, I>(bag: Aliq.FlatMap<T, I>): Iterable<T> {
-        return I.flatMap(get(bag.input), bag.func)
-    }
-    function createProduct<T, A, B>(bag: Aliq.Product<T, A, B>): Iterable<T> {
-        const a = get(bag.inputA)
-        const b = get(bag.inputB)
-        return I.flatMap(a, av => I.flatMap(b, bv => bag.func(av, bv)))
-    }
-    function create<T>(bag: Aliq.Bag<T>): Iterable<T> {
-        switch (bag.type) {
-            case "const":
-                return [bag.value]
-            case "flatMap":
-                return createFlatMap(bag)
-            case "merge":
-                return I.flatMap(bag.inputs, get)
-            case "product":
-                return createProduct(bag)
-            case "groupBy":
-                return I.values(I.groupBy(get(bag.input), bag.getKey, bag.reduce))
-            case "externalInput":
-                throw "external input is not specified"
+    class Implementation implements BagMap {
+        set<T>(inputBag: Aliq.ExternalInput<T>, input: Iterable<T>): void {
+            cache.set(inputBag, input)
+        }
+        get<T>(bag: Aliq.Bag<T>): Iterable<T> {
+            const cached = mapGet(bag)
+            if (cached === undefined) {
+                const created = this.create(bag)
+                cache.set(bag, created)
+                return created
+            } else {
+                return cached
+            }
+        }
+        create<T>(bag: Aliq.Bag<T>): Iterable<T> {
+            switch (bag.type) {
+                case "const":
+                    return [bag.value]
+                case "flatMap":
+                    return this.createFlatMap(bag)
+                case "merge":
+                    return I.flatMap(bag.inputs, this.get)
+                case "product":
+                    return this.createProduct(bag)
+                case "groupBy":
+                    return this.createGroupBy(bag)
+                case "externalInput":
+                    throw "external input is not specified"
+            }
+        }
+        createGroupBy<T, V>(bag: Aliq.GroupBy<T, V>): Iterable<T> {
+            const entries = I.entries(I.groupBy(this.get(bag.input), bag.reduce))
+            return I.flatMap(entries, bag.flatMap)
+        }
+        createProduct<T, A, B>(bag: Aliq.Product<T, A, B>): Iterable<T> {
+            const a = this.get(bag.inputA)
+            const b = this.get(bag.inputB)
+            return I.flatMap(a, av => I.flatMap(b, bv => bag.func(av, bv)))
+        }
+        createFlatMap<T, I>(bag: Aliq.FlatMap<T, I>): Iterable<T> {
+            return I.flatMap(this.get(bag.input), bag.func)
         }
     }
-    function get<T>(bag: Aliq.Bag<T>): Iterable<T> {
-        const cached = mapGet(bag)
-        if (cached === undefined) {
-            const created = create(bag)
-            cache.set(bag, created)
-            return created
-        } else {
-            return cached
-        }
-    }
-    return {
-        set: cache.set,
-        get: get,
-    }
+    return new Implementation()
 }
